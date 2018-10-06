77ea3275e83ee92abe98c0c7f3b60bd2197a78fc
wverbeke <willem.verbeke@cern.ch>
2018-10-06 18:00:29 +0200

    small fixes

--- diagnosticPlotting.py
+++ diagnosticPlotting.py
@@ -5,15 +5,15 @@ def plotComparison( training_history, network_name, metric ):
     #extract history of the metric 
     training_metric = training_history.history[metric]
     validation_metric = training_history.history['val_' + metric]
-    
+
     #plot metric as a function of the training epoch
-    epochs = range( len(training_metric) + 1 )
+    epochs = range(1, len(training_metric) + 1 )
     plt.plot(epochs, training_metric, 'b', label = 'training ' + metric ) # this name can be improved 
     plt.plot(epochs, validation_metric, 'r', label = 'validation ' + metric ) #this name can be improved 
     plt.legend()
     plt.xlabel('Epoch')
     plt.ylabel(metric) #this name can be improved 
-    plt.savefig( network_name + '.pdf')
+    plt.savefig( metric + '_' + network_name + '.pdf')
     
     #clear canvas
     plt.clf()
