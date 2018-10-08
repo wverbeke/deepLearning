fc00aee5f8aaf585741fbe0b464a05f366feea3a
wverbeke <willem.verbeke@cern.ch>
2018-10-08 19:07:28 +0200

    clean up old test code

--- trainKerasModel.py
+++ trainKerasModel.py
@@ -16,6 +16,15 @@ def tensorFlowSetNumThreads( num_threads ):
     K.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_parallelism_threads = num_threads, inter_op_parallelism_threads = num_threads ) ) )
 
 
+#make name for model depending on its hyperparameters 
+def denseModelName(num_hidden_layers, units_per_layer, activation, learning_rate, dropoutFirst, dropoutAll):
+    model_name = 'model_{0}hiddenLayers_{1}unitsPerLayer_{2}_learningRate{3}'.format(num_hidden_layers, units_per_layer, activation, learning_rate)  
+    model_name = model_name.replace('.', 'p')
+    model_name += ( '_dropoutFirst' if dropoutFirst else '' )
+    model_name += ( '_dropoutAll' if dropoutAll else '' )
+    return model_name
+
+
 def trainDenseClassificationModel(train_data, train_labels, validation_data, validatation_labels, train_weights = None, validation_weights = None, num_hidden_layers = 5, units_per_layer = 256, activation = 'relu', learning_rate = 0.0001, dropoutFirst=True, dropoutAll=False, dropoutRate = 0.5, num_epochs = 20, num_threads = 1):
 
     model = models.Sequential()
@@ -46,29 +55,26 @@ def trainDenseClassificationModel(train_data, train_labels, validation_data, val
     )
 
     #name of file in which model will be saved
-    model_output_name = 'model_{0}hiddenLayers_{1}unitsPerLayer_{2}_learningRate{3}'.format(num_hidden_layers, units_per_layer, activation, learning_rate)  
-    model_output_name += ( '_dropoutFirst' if dropoutFirst else '' )
-    model_output_name += ( '_dropoutAll' if dropoutAll else '' )
-    model_output_name += '.h5'
+    model_output_name = denseModelName( num_hidden_layers, units_per_layer, activation, learning_rate, dropoutFirst, dropoutAll)
 
     #cut off training at convergence and save model with best validation
     callbacks_list = [
         callbacks.EarlyStopping(
             monitor = 'acc',
-            patience = 2
+            patience = 4
         ),
         callbacks.ModelCheckpoint(
             monitor = 'val_acc',
-            filepath = model_output_name,
+            filepath = model_output_name + '.h5',
             save_best_only = True
         )
     ]
 
-            
+    #set number of threads to use in training        
     tensorFlowSetNumThreads( num_threads ) 
 
     #train model
-    model.fit(
+    training_history = model.fit(
         train_data,
         train_labels,
         sample_weight = (None if train_weights.size == 0 else train_weights),
@@ -78,66 +84,15 @@ def trainDenseClassificationModel(train_data, train_labels, validation_data, val
         callbacks = callbacks_list
     )
 
-    return model 
+    #plot loss and accuracy as a function of epochs
+    plotAccuracyComparison( training_history, model_output_name )
+    plotLossComparison( training_history, model_output_name )
+
+    #output name of model so that caller can access the correct file and load the best (saved) model
+    return model_output_name
 
 
    
 if __name__ == '__main__':
-    testFile = TFile("ttW_trainingData_new.root")
-    testFile.ls()
-    signalTree = testFile.Get('signalTree')
-    backgroundTree = testFile.Get('bkgTree')
-    branchList = [ branch.GetName() for branch in signalTree.GetListOfBranches() ]
-    etaBranches = [ name for name in branchList if 'Eta' in name ]
-    branchList = [ name for name in branchList if not 'Charge' in name ]
-    branchList = [ name for name in branchList if not 'E' in name ]
-    branchList += etaBranches
-    branchList.remove('_weight')
-
-    training_data_signal = treeToArray( signalTree, branchList )
-    training_weights_signal = treeToArray( signalTree, '_weight' )
-    training_labels_signal = np.ones( len(training_data_signal) )
-
-    training_data_background = treeToArray( backgroundTree, branchList)
-    training_weights_background = treeToArray( backgroundTree, '_weight' )
-    training_labels_background = np.zeros( len(training_data_background) )
-
-    training_data = np.concatenate( (training_data_signal, training_data_background) , axis = 0)
-    training_weights = np.concatenate( (training_weights_signal, training_weights_background ), axis = 0)
-    training_labels = np.concatenate( (training_labels_signal, training_labels_background) , axis = 0)
-
-    indices = list( range( len( training_data ) ) )
-    np.random.shuffle( indices )
-
-    training_data = training_data[indices] 
-    training_weights = training_weights[indices] 
-    training_labels = training_labels[indices]
-    
-    validation_split = 0.5
-    split_index = int( len( training_data )*validation_split )
-    validation_data = training_data[: split_index ]
-    validation_weights = training_weights[: split_index ]
-    validation_labels = training_labels[: split_index ]
-
-    training_data = training_data[split_index : ]
-    training_weights = training_weights[split_index : ]
-    training_labels = training_labels[split_index : ]
-
-    model = trainDenseClassificationModel( training_data, training_labels, validation_data, validation_labels, training_weights, validation_weights, num_threads = 4, learning_rate = 0.001, dropoutFirst = False, num_epochs = 1)
-    
-    signal_output = model.predict(  training_data_signal )
-    background_output = model.predict( training_data_background )
-
-    testOutputPlot( signal_output, training_weights_signal )
-    
-    #eff_signal, eff_bkg = computeROC( signal_output, training_weights_signal, background_output, training_weights_background )
-    #
-    #plotROC( eff_signal, eff_bkg )
-    #
-    #print( 'ROC integral = {}'.format(areaUndeCurve( eff_signal, eff_bkg )	) )
-
-    
-
-    
-
+    pass
 
