443892a4e7e25a3bcde0f57bedabc39914b843e5
wverbeke <willem.verbeke@cern.ch>
2018-10-09 20:21:49 +0200

    modify runTraining script to need input file

--- runTraining.py
+++ runTraining.py
@@ -1,58 +1,36 @@
-#name of input root file, relative to the directory of this script
-root_file_name = 'ttW_trainingData_new.root'
-
-#names of trees that contain signal and background events 
-signal_tree = 'signalTree'
-background_tree = 'bkgTree'
-
-#list of variables to be used in training (corresponding to branches in the tree)
-branch_list = [
-	'_lepPt1', '_lepEta1', '_lepPhi1',
-	'_lepPt2', '_lepEta2', '_lepPhi2',
-	'_jetPt1', '_jetEta1', '_jetPhi1', '_jetCSV1',
-	'_jetPt2', '_jetEta2', '_jetPhi2', '_jetCSV2',
-	'_jetPt3', '_jetEta3', '_jetPhi3', '_jetCSV3',
-	'_jetPt4', '_jetEta4', '_jetPhi4', '_jetCSV4',
-	'_jetPt5', '_jetEta5', '_jetPhi5', '_jetCSV5',
-	'_jetPt6', '_jetEta6', '_jetPhi6', '_jetCSV6',
-	'_metPt1', '_metPhi1'
-	]
-
-#branch that indicates the event weights 
-weight_branch = '_weight'
-
-#use only positive weights in training or not 
-only_positive_weights = True
-
-#validation and test fractions
-validation_fraction = 0.4
-test_fraction = 0.2
-
-#different neural network parameters to test
-num_hidden_layers = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-units_per_layer = [64, 128, 256, 512]
-learning_rates = [0.001, 0.0001, 0.00001]
-dropout_first = [False, True]
-dropout_all = [False, True] 
-dropout_rate = [0.5, 0.3]
-
-
 import os
 import sys
 import argparse
+
+#check if atleast one additional argument is given
+if len( sys.argv ) < 2:
+    print( 'Error: incorrect number of arguments given to script.')
+    print( 'Usage: <python runTraining.py configuration.py>')
+    sys.exit()
+
+#read input file 
+configuration_file = __import__(sys.argv[1].replace('.py', '') )
 
 from Dataset import Data
 from jobSubmission import * 
 from trainKerasModel import denseModelName
 
-
 def trainAndEvaluateModel( num_hidden_layers, units_per_layer, learning_rate, dropout_first, dropout_all, dropout_rate):
 
     #make sure correct path is given for input root file
-    root_file_name_full = os.path.join( os.path.dirname(os.path.abspath( __file__) ) , root_file_name )
+    root_file_name_full = os.path.join( os.path.dirname(os.path.abspath( __file__) ) , configuration_file.root_file_name )
 
     #train model
-    classification_data = Data(root_file_name_full, signal_tree, background_tree, branch_list, weight_branch, validation_fraction, test_fraction, only_positive_weights )
+    classification_data = Data(
+        root_file_name_full, 
+        configuration_file.signal_tree, 
+        configuration_file.background_tree, 
+        configuration_file.branch_list, 
+        configuration_file.weight_branch, 
+        configuration_file.validation_fraction, 
+        configuration_file.test_fraction, 
+        configuration_file.only_positive_weights
+    )
     classification_data.trainDenseClassificationModel(
     	num_hidden_layers = num_hidden_layers, 
     	units_per_layer = units_per_layer, 
@@ -97,7 +75,7 @@ def submitTrainingJob(num_hidden_layers, units_per_layer, learning_rate, dropout
  
 if __name__ == '__main__' :
 
-    if len( sys.argv ) > 1:    
+    if len( sys.argv ) > 2:    
         parser = argparse.ArgumentParser()
         parser.add_argument('num_hidden_layers', type=int)
         parser.add_argument('units_per_layer', type=int)
@@ -110,8 +88,9 @@ if __name__ == '__main__' :
         
         trainAndEvaluateModel( args.num_hidden_layers, args.units_per_layer, args.learning_rate, args.dropout_first, args.dropout_last, args.dropout_rate)        
 
-    else:
-        num_networks = len( num_hidden_layers )*len( units_per_layer )*len( learning_rates )*len( dropout_first )*len( dropout_all )*len( dropout_rate )
+    else :
+        num_networks = len( configuration_file.num_hidden_layers )*len( configuration_file.units_per_layer )*len(  configuration_file.learning_rates )*len(  configuration_file.dropout_first )*len( configuration_file.dropout_all )*len( configuration_file.dropout_rate )
+
         print( 'Number of neural networks to be trained = {}'.format( num_networks ) )
         if num_networks > 10000:
             print( 'Error : requesting to train more than 10000 neural networks. This will be too much for the T2 cluster to handle.' )
@@ -119,11 +98,10 @@ if __name__ == '__main__' :
             sys.exit()
 
         #submit a job for each variation of the neural network parameters
-        for num_hidden_layers_var in num_hidden_layers:
-            for units_per_layer_var in units_per_layer:
-                for learning_rate_var in learning_rates:
-                    for dropout_first_var in dropout_first:
-                        for dropout_all_var in dropout_all:
-                            for dropout_rate_var in dropout_rate:
+        for num_hidden_layers_var in configuration_file.num_hidden_layers:
+            for units_per_layer_var in configuration_file.units_per_layer:
+                for learning_rate_var in configuration_file.learning_rates:
+                    for dropout_first_var in configuration_file.dropout_first:
+                        for dropout_all_var in configuration_file.dropout_all:
+                            for dropout_rate_var in configuration_file.dropout_rate:
                                 submitTrainingJob(num_hidden_layers_var, units_per_layer_var, learning_rate_var, dropout_first_var, dropout_all_var, dropout_rate_var) 
-                        
