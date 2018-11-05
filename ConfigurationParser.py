f18374f6dbfac8fa27cc88eb8b75fab9096eb590
wverbeke <willem.verbeke@cern.ch>
2018-11-05 16:26:15 +0100

    add optimizer class to facilitate the inclusion of several optimizers in the hyperparameter scans

--- ConfigurationParser.py
+++ ConfigurationParser.py
@@ -1,11 +1,77 @@
 import itertools
 
+class OptimizerInfo
+
+class Optimizer():
+    optimizer_info = [
+        #optimizer name, keras optimizer class, default learning rate, default decay rate 
+        ('RMSprop', keras.optimizers.RMSprop, 0.001, 0.0),
+        ('Adagrad', keras.optimizers.Adagrad, 0.01, 0.0),
+        ('Adadelta', keras.optimizers.Adadelta, 1.0, 0.0),
+        ('Adam', keras.optimizers.Adam, 0.001, 0.0),
+        ('Adamax', keras.optimizers.Adamax, 0.002, 0.0),
+        ('Nadam', keras.optimizers.Nadam, 0.002, 0.004)
+    ]
+
+    def __init__(optimizer_name, relative_learning_rate, relative_learning_rate_decay):
+        
+        #find correct index 
+        index = 9999
+        for i, entry in enumerate(optimizer_info):
+            if optimizer_name == entry[0]:
+                index = i
+        if index == 9999:
+            print('Error in Optimizer::__init__ : optimizer {} is not known. Returning control.'.format( optimizer_name ) )
+            return
+        
+        self.optimizer_name = optimizer_name 
+        
+        default_learning_rate = optimizer_info[index][2]
+        self.learning_rate = relative_learning_rate*default_learning_rate 
+            
+        default_learing_rate_decay = optimizer_info[index][3] 
+        keras_optimizer = optimizer_info[index][1]
+        if optimizer_name == 'Nadam' : 
+            if relative_learning_rate_decay == 0:
+                relative_learning_rate_decay = 1
+            self.optimizer = keras_optimizer( lr = learning_rate, schedule_decay = default_learning_rate*relative_learning_rate_decay)
+        else :
+            self.optimizer = keras_optimizer( lr = learning_rate, decay = relative_learning_rate_decay) 
+
+    
+    def getOptimizer(self):
+        return self.optimizer
+
+    def getName(self):
+        name = self.optimizer_name 
+        name += '_learningRate{}'.format( self.learning_rate )
+		name += ( '_learningRateDecay{}'.format( self.learning_rate_decay ) if (learning_rate_decay != d
+
+	 model_name = 'model_{0}hiddenLayers_{1}unitsPerLayer_{2}_learningRate{3}'.format(num_hidden_layers, units_per_layer, activation, learning_rate)
+    model_name += ( '_learningRateDecay{}'.format(learning_rate_decay) if (learning_rate_decay > 0) else '' )
+    model_name += ( '_dropoutFirst{}'.format(dropout_rate) if dropout_first else '' )
+    model_name += ( '_dropoutAll{}'.format(dropout_rate) if dropout_all else '' )
+
+
+    
+    def __eq__(self, rhs):
+        return ( self.optimizer == self.optimizer )
+
+
+    def __ne__(self, rhs):
+        return not( self.__eq__(rhs) )
+
+
+    def __hash__(self):
+        return hash( self.optimizer )
+
+
+
 class HyperParameterSet():
-    def __init__(self, num_hidden_layers, units_per_layer, learning_rate, learning_rate_decay, dropout_first, dropout_all, dropout_rate):
+    def __init__(self, num_hidden_layers, units_per_layer, optimizer_name, learning_rate, learning_rate_decay, dropout_first, dropout_all, dropout_rate):
         self.num_hidden_layers = num_hidden_layers
         self.units_per_layer = units_per_layer
-        self.learning_rate = learning_rate
-        self.learning_rate_decay = learning_rate_decay
+        self.optimizer = Optimizer( optimizer_name, learning_rate, learning_rate_decay ), 
         self.dropout_first = dropout_first
         self.dropout_all = dropout_all
         self.dropout_rate = dropout_rate
@@ -19,7 +85,7 @@ class HyperParameterSet():
 
 
     def getParameterSet(self):
-        return ( self.num_hidden_layers, self.units_per_layer, self.learning_rate, self.learning_rate_decay, self.dropout_first, self.dropout_all, self.dropout_rate )
+        return ( self.num_hidden_layers, self.units_per_layer, self.optimizer.getOptimizer(), self.dropout_first, self.dropout_all, self.dropout_rate )
 
 
     def __eq__(self, rhs):
---
+++ Optimizer.py
@@ -0,0 +1,80 @@
+import keras
+
+class OptimizerInfo():
+    #optimizer name, keras optimizer class, default learning rate, default decay rate 
+    info = {
+        'RMSprop' : (keras.optimizers.RMSprop, 0.001, 0.0),
+        'Adagrad' : (keras.optimizers.Adagrad, 0.01, 0.0),
+        'Adadelta': (keras.optimizers.Adadelta, 1.0, 0.0),
+        'Adam' : (keras.optimizers.Adam, 0.001, 0.0),
+        'Adamax' : (keras.optimizers.Adamax, 0.002, 0.0),
+        'Nadam': (keras.optimizers.Nadam, 0.002, 0.004),
+    }
+ 
+    def __init__(self, optimizer_name):
+        if not (optimizer_name in info ):
+            print( 'Error in OptimizerInfo::__init__() : optimizer {} not found. Returning control.'.format( optimizer_name ) )
+        self.keras_optimizer = info[optimizer_name][0]
+        self.default_learning_rate = info[optimizer_name][1]
+        self.default_decay = info[optimizer_name][2]
+    
+    
+    def defaultLearningRate(self):
+        return self.default_learning_rate
+    
+    
+    def defaultDecay(self):
+    	return self.default_decay
+    
+    
+    def kerasOptimizer(self):
+    	return self.keras_optimizer
+   
+ 
+
+class Optimizer():
+
+    def __init__(self, optimizer_name, relative_learning_rate, relative_learning_rate_decay):
+        
+        #make object of helper class to retrieve default information of optimizer
+        optimizer_info = OptimizerInfo(optimizer_name)
+        
+        #set learning rate 
+        self.learning_rate = relative_learning_rate*optimizer_info.defaultLearningRate()
+        
+        #set decay 
+        default_decay = optimizer_info.defaultDecay()
+        self.learning_rate_decay = relative_learning_rate_decay*default_decay
+        
+        #set keras optimizer 
+        keras_optimizer = optimizer_info.kerasOptimizer()
+        if optimizer_name == 'Nadam' :
+            self.optimizer = keras_optimizer( lr = self.learning_rate, schedule_decay = self.learning_rate_decay )
+        else :
+        	self.optimizer = keras_optimizer( lr = self.learning_rate, decay = self.learning_rate_decay )
+        
+        #form optimizer name 
+        self.name = optimizer_name 
+        self.name += '_learningRate{}'.format( self.learning_rate )
+        self.name += ( '_learningRateDecay{}'.format( self.learning_rate_decay ) if (self.learning_rate_decay != default_decay) else '')
+
+
+    def optimizer(self):
+        return self.optimizer
+    
+    
+    def name(self):
+    	return self.name
+    	
+    
+    def __eq__(self, rhs):
+        return ( self.optimizer == self.optimizer )
+    
+    
+    def __ne__(self, rhs):
+        return not( self.__eq__(rhs) )
+    
+    
+    def __hash__(self):
+        return hash( self.optimizer )
+
